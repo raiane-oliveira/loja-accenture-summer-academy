9e01ede3a604ae53a3f6fbe625901a73f37aac04
Bruna Neves <118817129+ibrunaneves@users.noreply.github.com>
2025-01-21 21:24:32 -0300

    Vendedor.java

--- src/main/java/com/academia/loja_accenture/modulos/usuario/domain/Vendedor.java
+++ src/main/java/com/academia/loja_accenture/modulos/usuario/domain/Vendedor.java
@@ -5,37 +5,8 @@ package com.academia.loja_accenture.modulos.usuario.domain;
  *
  * @author Bruna Neves
  */
-
 public class Vendedor {
     private int id;
     private String nome;
     private String setor;
-
-    public Vendedor() {}
-
-    public Vendedor(String nome, String setor) {
-        this.nome = nome;
-        this.setor = setor;
-    }
-
-    public int getId() {
-        return id;
-    }
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getSetor() {
-        return setor;
-    }
-    public void setSetor(String setor) {
-        this.setor = setor;
-    }
 }
